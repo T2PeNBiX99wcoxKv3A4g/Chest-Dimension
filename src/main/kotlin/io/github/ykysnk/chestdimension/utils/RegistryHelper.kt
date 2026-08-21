6b6d5bf91cdb804b37daa0a8651d90e111fc3e75
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-22 07:22:51 +0800

    feat: Add RegistryHelper utility class for managing registrable items

---
+++ src/main/kotlin/io/github/ykysnk/chestdimension/utils/RegistryHelper.kt
@@ -0,0 +1,13 @@
+package io.github.ykysnk.chestdimension.utils
+
+import io.github.ykysnk.chestdimension.Constants.funcAndListAdd
+
+open class RegistryHelper<T> {
+    private val mutableAll = mutableListOf<T>()
+
+    @Suppress("unused")
+    val all: List<T>
+        get() = mutableAll.toList()
+
+    protected open fun register(func: () -> T) = mutableAll.funcAndListAdd(func)
+}
