d5da5e1182edd95db2da3e90e515fe172cdb9ce5
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-23 08:54:35 +0800

    feat: Add `give-chest` command and improve UUID-based teleportation logic

--- src/main/kotlin/io/github/ykysnk/chestdimension/command/ChestDimCommand.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/command/ChestDimCommand.kt
@@ -2,18 +2,25 @@ package io.github.ykysnk.chestdimension.command
 
 import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.arguments.StringArgumentType
+import com.mojang.brigadier.builder.RequiredArgumentBuilder
 import io.github.ykysnk.chestdimension.Constants
+import io.github.ykysnk.chestdimension.block.entity.BlockEntityTypes
 import io.github.ykysnk.chestdimension.extensions.teleportToLevel
+import io.github.ykysnk.chestdimension.item.Items
 import io.github.ykysnk.chestdimension.level.ChestLevelManager
 import io.github.ykysnk.chestdimension.level.UUIDManager
 import net.minecraft.commands.CommandSourceStack
 import net.minecraft.commands.Commands
 import net.minecraft.commands.arguments.DimensionArgument
+import net.minecraft.commands.arguments.EntityArgument
 import net.minecraft.commands.arguments.coordinates.Vec3Argument
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.network.chat.ClickEvent
 import net.minecraft.network.chat.Component
 import net.minecraft.network.chat.HoverEvent
 import net.minecraft.network.chat.MutableComponent
+import net.minecraft.world.item.BlockItem
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.phys.Vec3
 import java.util.*
 
@@ -31,11 +38,17 @@ object ChestDimCommand {
                 }
 
                 val uuidComponent: MutableComponent = Component.literal(uuid.toString()).withStyle {
-                    it.withUnderlined(true).withClickEvent(ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, uuid.toString()))
-                        .withHoverEvent(HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Click to copy UUID")))
+                    it.withUnderlined(true)
+                        .withClickEvent(ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, uuid.toString()))
+                        .withHoverEvent(
+                            HoverEvent(
+                                HoverEvent.Action.SHOW_TEXT,
+                                Component.literal("Click to copy UUID")
+                            )
+                        )
                 }
                 val world = ChestLevelManager.getOrCreate(context.source.server, uuid)
-                player.teleportToLevel(world, Vec3(0.5,1.0,0.5))
+                player.teleportToLevel(world, Vec3(0.5, 1.0, 0.5))
                 context.source.sendSuccess({ Component.literal("Created world: ").append(uuidComponent) }, true)
                 1
             }.getOrElse {
@@ -45,14 +58,13 @@ object ChestDimCommand {
             }
         })
 
-        val builder2 = Commands.literal("tp")
-
-        for ((uuid) in UUIDManager.get()) {
-            builder2.then(
-                Commands.literal(uuid).then(Commands.argument("pos", Vec3Argument.vec3()).executes { context ->
+        builder.then(
+            Commands.literal("tp")
+                .then(uuidArg.then(Commands.argument("pos", Vec3Argument.vec3()).executes { context ->
                     val player = context.source.playerOrException
                     val pos = Vec3Argument.getVec3(context, "pos")
-                    val uuid = runCatching { UUID.fromString(uuid) }.getOrElse {
+                    val uuidString = StringArgumentType.getString(context, "uuid")
+                    val uuid = runCatching { UUID.fromString(uuidString) }.getOrElse {
                         Constants.LOGGER.error(it.localizedMessage, it)
                         context.source.sendFailure(Component.literal("UUID is not valid."))
                         return@executes 0
@@ -60,11 +72,8 @@ object ChestDimCommand {
                     val level = ChestLevelManager.getOrCreate(context.source.server, uuid)
                     player.teleportToLevel(level, pos)
                     1
-                })
-            )
-        }
-
-        builder.then(builder2)
+                }))
+        )
 
         builder.then(
             Commands.literal("tp-default").then(
@@ -85,24 +94,36 @@ object ChestDimCommand {
         )
 
         builder.then(
-            Commands.literal("tp-raw").then(
-                Commands.argument("uuid", StringArgumentType.string())
-                    .then(Commands.argument("pos", Vec3Argument.vec3()).executes { context ->
-                        val player = context.source.playerOrException
-                        val name = StringArgumentType.getString(context, "uuid")
-                        val uuid = runCatching { UUID.fromString(name) }.getOrElse {
-                            Constants.LOGGER.error(it.localizedMessage, it)
-                            context.source.sendFailure(Component.literal("UUID is not valid."))
-                            return@executes 0
-                        }
-                        val level = ChestLevelManager.getOrCreate(context.source.server, uuid)
-                        val pos = Vec3Argument.getVec3(context, "pos")
-                        player.teleportToLevel(level, pos)
-                        1
-                    })
-            )
+            Commands.literal("give-chest")
+                .then(uuidArg.then(Commands.argument("player", EntityArgument.player()).executes { context ->
+                    val player = EntityArgument.getPlayer(context, "player")
+                        ?: throw CommandSourceStack.ERROR_NOT_PLAYER.create()
+                    val uuidString = StringArgumentType.getString(context, "uuid")
+                    val uuid = runCatching { UUID.fromString(uuidString) }.getOrElse {
+                        Constants.LOGGER.error(it.localizedMessage, it)
+                        context.source.sendFailure(Component.literal("UUID is not valid."))
+                        return@executes 0
+                    }
+                    player.addItem(createChestItem(uuid))
+                    1
+                }))
         )
 
         dispatcher.register(builder)
+    }
+
+    private val uuidArg: RequiredArgumentBuilder<CommandSourceStack, String> by lazy {
+        Commands.argument("uuid", StringArgumentType.word()).suggests { _, builder ->
+            UUIDManager.get().keys.forEach(builder::suggest)
+            builder.buildFuture()
+        }
+    }
+
+    private fun createChestItem(uuid: UUID): ItemStack {
+        val stack = ItemStack(Items.CHEST_DIMENSION)
+        val tag = CompoundTag()
+        tag.putUUID("UUID", uuid)
+        BlockItem.setBlockEntityData(stack, BlockEntityTypes.CHEST_DIMENSION, tag)
+        return stack
     }
 }
