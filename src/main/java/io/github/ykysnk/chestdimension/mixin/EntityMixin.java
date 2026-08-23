9b3d189cf1be75fba848605aec2fb1b2b422109f
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-23 09:04:09 +0800

    feat: Add `EntityMixin` to handle entity teleportation below world

---
+++ src/main/java/io/github/ykysnk/chestdimension/mixin/EntityMixin.java
@@ -0,0 +1,27 @@
+package io.github.ykysnk.chestdimension.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import io.github.ykysnk.chestdimension.Constants;
+import io.github.ykysnk.chestdimension.level.ChestLevelManager;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+@SuppressWarnings({"AbstractClassWithoutAbstractMethods", "AbstractClassNeverImplemented"})
+@Mixin(Entity.class)
+public abstract class EntityMixin {
+    @Shadow
+    private Level level;
+
+    @WrapOperation(method = "checkBelowWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;onBelowWorld()V"))
+    private void checkBelowWorld(Entity instance, Operation<Void> original) {
+        var test = ChestLevelManager.INSTANCE.teleportEntityToExit(level, instance);
+        if (!level.isClientSide)
+            Constants.LOGGER.info("test: {}", test);
+        if (test) return;
+        original.call(instance);
+    }
+}
