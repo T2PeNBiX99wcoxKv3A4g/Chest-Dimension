3ceb2cefa5e2900e73cd4778eabaff261c3c8bec
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-24 03:25:21 +0800

    feat: Add `chest_platform_enter_plate` item registration

--- src/main/kotlin/io/github/ykysnk/chestdimension/item/Items.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/item/Items.kt
@@ -10,6 +10,7 @@ object Items : RegistryHelper<Item>() {
     val CHEST_PLATFORM: Item = register { MCItems.registerBlock(Blocks.CHEST_PLATFORM) }
     val CHEST_PLATFORM_WALL: Item = register { MCItems.registerBlock(Blocks.CHEST_PLATFORM_WALL) }
     val CHEST_PLATFORM_FENCE: Item = register { MCItems.registerBlock(Blocks.CHEST_PLATFORM_FENCE) }
+    val CHEST_PLATFORM_ENTER_PLATE: Item = register { MCItems.registerBlock(Blocks.CHEST_PLATFORM_ENTER_PLATE) }
     val TELEPORT_PRESSURE_PLATE: Item = register { MCItems.registerBlock(Blocks.TELEPORT_PRESSURE_PLATE) }
     val CHEST_DIMENSION: Item = register { MCItems.registerBlock(Blocks.CHEST_DIMENSION) }
 }
