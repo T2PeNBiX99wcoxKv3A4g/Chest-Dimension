393678f144e93539f5cfb096ba80f87fb347c360
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-23 09:04:48 +0800

    feat: Add `ChestDimensionBlock` import to `ChestLevelManager` for enhanced block management

--- src/main/kotlin/io/github/ykysnk/chestdimension/level/ChestLevelManager.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/level/ChestLevelManager.kt
@@ -2,6 +2,7 @@ package io.github.ykysnk.chestdimension.level
 
 import io.github.ykysnk.chestdimension.Constants
 import io.github.ykysnk.chestdimension.block.Blocks
+import io.github.ykysnk.chestdimension.block.ChestDimensionBlock
 import io.github.ykysnk.chestdimension.data.LevelData
 import io.github.ykysnk.chestdimension.extensions.*
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
