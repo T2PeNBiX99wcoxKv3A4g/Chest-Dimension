8eda1108926a7b3b05d558422fb1bdeb50b5ae7f
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-24 01:37:28 +0800

    refactor: Remove unused `CompoundTag` methods from `TeleportPressurePlateBlockEntity`

--- src/main/kotlin/io/github/ykysnk/chestdimension/block/entity/TeleportPressurePlateBlockEntity.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/block/entity/TeleportPressurePlateBlockEntity.kt
@@ -2,7 +2,6 @@ package io.github.ykysnk.chestdimension.block.entity
 
 import io.github.ykysnk.chestdimension.level.ChestLevelManager
 import net.minecraft.core.BlockPos
-import net.minecraft.nbt.CompoundTag
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
@@ -10,12 +9,6 @@ import net.minecraft.world.level.block.state.BlockState
 class TeleportPressurePlateBlockEntity(pos: BlockPos, blockState: BlockState) :
     BlockEntity(BlockEntityTypes.TELEPORT_PRESSURE_PLATE, pos, blockState) {
     private val entitiesCache: HashSet<Entity> = HashSet()
-
-    override fun load(tag: CompoundTag) {
-    }
-
-    override fun saveAdditional(tag: CompoundTag) {
-    }
 
     fun addEntity(entity: Entity) {
         entitiesCache.add(entity)
