46d71e30be1c3d12bb764624d5b9cf8b9f02d1f7
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-26 04:31:41 +0800

    feat: Add time management commands to `ChestDimCommand`, including set, add, and query options

--- src/main/kotlin/io/github/ykysnk/chestdimension/command/ChestDimCommand.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/command/ChestDimCommand.kt
@@ -1,6 +1,7 @@
 package io.github.ykysnk.chestdimension.command
 
 import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.arguments.IntegerArgumentType
 import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.builder.RequiredArgumentBuilder
 import io.github.ykysnk.chestdimension.Constants
@@ -13,17 +14,22 @@ import net.minecraft.commands.CommandSourceStack
 import net.minecraft.commands.Commands
 import net.minecraft.commands.arguments.DimensionArgument
 import net.minecraft.commands.arguments.EntityArgument
+import net.minecraft.commands.arguments.TimeArgument
 import net.minecraft.commands.arguments.coordinates.Vec3Argument
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.network.chat.ClickEvent
 import net.minecraft.network.chat.Component
 import net.minecraft.network.chat.HoverEvent
 import net.minecraft.network.chat.MutableComponent
+import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.ItemStack
 import java.util.*
 
 object ChestDimCommand {
+    private const val THIS = "this"
+    private const val ALL = "*"
+
     fun register(dispatcher: CommandDispatcher<CommandSourceStack>) {
         val builder = Commands.literal("chestdim").requires { it.hasPermission(2) }
 
@@ -57,15 +63,29 @@ object ChestDimCommand {
                 .then(uuidArg.then(Commands.argument("pos", Vec3Argument.vec3()).executes { context ->
                     val player = context.source.playerOrException
                     val pos = Vec3Argument.getVec3(context, "pos")
-                    val uuidString = StringArgumentType.getString(context, "uuid")
-                    val uuid = runCatching { UUID.fromString(uuidString) }.getOrElse {
-                        Constants.LOGGER.error(it.localizedMessage, it)
-                        context.source.sendFailure(Component.literal("UUID is not valid."))
-                        return@executes 0
+                    when (val uuidString = StringArgumentType.getString(context, "uuid")) {
+                        THIS -> {
+                            val uuid = ChestLevelManager.findUUIDByLevel(context.source.level)
+                            if (uuid == null) {
+                                context.source.sendFailure(Component.literal("Can't find any UUID using this level."))
+                                return@executes 0
+                            }
+                            val level = ChestLevelManager.getOrCreate(context.source.server, uuid)
+                            player.teleportToLevel(level, pos)
+                            1
+                        }
+
+                        else -> {
+                            val uuid = runCatching { UUID.fromString(uuidString) }.getOrElse {
+                                Constants.LOGGER.error(it.localizedMessage, it)
+                                context.source.sendFailure(Component.literal("UUID is not valid."))
+                                return@executes 0
+                            }
+                            val level = ChestLevelManager.getOrCreate(context.source.server, uuid)
+                            player.teleportToLevel(level, pos)
+                            1
+                        }
                     }
-                    val level = ChestLevelManager.getOrCreate(context.source.server, uuid)
-                    player.teleportToLevel(level, pos)
-                    1
                 }))
         )
 
@@ -92,30 +112,168 @@ object ChestDimCommand {
                 .then(uuidArg.then(Commands.argument("player", EntityArgument.player()).executes { context ->
                     val player = EntityArgument.getPlayer(context, "player")
                         ?: throw CommandSourceStack.ERROR_NOT_PLAYER.create()
-                    val uuidString = StringArgumentType.getString(context, "uuid")
-                    val uuid = runCatching { UUID.fromString(uuidString) }.getOrElse {
-                        Constants.LOGGER.error(it.localizedMessage, it)
-                        context.source.sendFailure(Component.literal("UUID is not valid."))
-                        return@executes 0
+                    when (val uuidString = StringArgumentType.getString(context, "uuid")) {
+                        THIS -> {
+                            val uuid = ChestLevelManager.findUUIDByLevel(context.source.level)
+                            if (uuid == null) {
+                                context.source.sendFailure(Component.literal("Can't find any UUID using this level."))
+                                return@executes 0
+                            }
+                            player.addItem(createChestItem(uuid))
+                            1
+                        }
+
+                        else -> {
+                            val uuid = runCatching { UUID.fromString(uuidString) }.getOrElse {
+                                Constants.LOGGER.error(it.localizedMessage, it)
+                                context.source.sendFailure(Component.literal("UUID is not valid."))
+                                return@executes 0
+                            }
+                            player.addItem(createChestItem(uuid))
+                            1
+                        }
                     }
-                    player.addItem(createChestItem(uuid))
-                    1
                 }))
         )
 
+        val timeBuilder = Commands.literal("time")
+        val setBuilder = Commands.literal("set")
+
+        setBuilder.then(Commands.literal("day").executes {
+            val uuidString = StringArgumentType.getString(it, "uuid")
+            setTime(uuidString, it.getSource(), 1000)
+        })
+
+        setBuilder.then(Commands.literal("noon").executes {
+            val uuidString = StringArgumentType.getString(it, "uuid")
+            setTime(uuidString, it.getSource(), 6000)
+        })
+
+        setBuilder.then(Commands.literal("night").executes {
+            val uuidString = StringArgumentType.getString(it, "uuid")
+            setTime(uuidString, it.getSource(), 13000)
+        })
+
+        setBuilder.then(Commands.literal("midnight").executes {
+            val uuidString = StringArgumentType.getString(it, "uuid")
+            setTime(uuidString, it.getSource(), 18000)
+        })
+
+        setBuilder.then(Commands.argument("time", TimeArgument.time()).executes {
+            val uuidString = StringArgumentType.getString(it, "uuid")
+            val time = IntegerArgumentType.getInteger(it, "time")
+            setTime(uuidString, it.getSource(), time)
+        })
+
+        val addBuilder = Commands.literal("add")
+
+        addBuilder.then(Commands.argument("time", TimeArgument.time()).executes {
+            val uuidString = StringArgumentType.getString(it, "uuid")
+            val time = IntegerArgumentType.getInteger(it, "time")
+            addTime(uuidString, it.getSource(), time)
+        })
+
+        val queryBuilder = Commands.literal("query")
+
+        queryBuilder.then(Commands.literal("daytime").executes {
+            when (val uuidString = StringArgumentType.getString(it, "uuid")) {
+                THIS -> {
+                    val uuid = ChestLevelManager.findUUIDByLevel(it.source.level)
+                    if (uuid == null) {
+                        it.source.sendFailure(Component.literal("Can't find any UUID using this level."))
+                        return@executes 0
+                    }
+                    val level = ChestLevelManager.getOrCreate(it.source.server, uuid)
+                    queryTime(it.getSource(), getDayTime(level))
+                }
+
+                else -> {
+                    val uuid = runCatching { UUID.fromString(uuidString) }.getOrElse { exception ->
+                        Constants.LOGGER.error(exception.localizedMessage, exception)
+                        it.source.sendFailure(Component.literal("UUID is not valid."))
+                        return@executes 0
+                    }
+                    val level = ChestLevelManager.getOrCreate(it.source.server, uuid)
+                    queryTime(it.getSource(), getDayTime(level))
+                }
+            }
+        })
+
+        queryBuilder.then(Commands.literal("gametime").executes {
+            when (val uuidString = StringArgumentType.getString(it, "uuid")) {
+                THIS -> {
+                    val uuid = ChestLevelManager.findUUIDByLevel(it.source.level)
+                    if (uuid == null) {
+                        it.source.sendFailure(Component.literal("Can't find any UUID using this level."))
+                        return@executes 0
+                    }
+                    val level = ChestLevelManager.getOrCreate(it.source.server, uuid)
+                    queryTime(it.getSource(), (level.gameTime % 2147483647L).toInt())
+                }
+
+                else -> {
+                    val uuid = runCatching { UUID.fromString(uuidString) }.getOrElse { exception ->
+                        Constants.LOGGER.error(exception.localizedMessage, exception)
+                        it.source.sendFailure(Component.literal("UUID is not valid."))
+                        return@executes 0
+                    }
+                    val level = ChestLevelManager.getOrCreate(it.source.server, uuid)
+                    queryTime(it.getSource(), (level.gameTime % 2147483647L).toInt())
+                }
+            }
+        })
+
+        queryBuilder.then(Commands.literal("day").executes {
+            when (val uuidString = StringArgumentType.getString(it, "uuid")) {
+                THIS -> {
+                    val uuid = ChestLevelManager.findUUIDByLevel(it.source.level)
+                    if (uuid == null) {
+                        it.source.sendFailure(Component.literal("Can't find any UUID using this level."))
+                        return@executes 0
+                    }
+                    val level = ChestLevelManager.getOrCreate(it.source.server, uuid)
+                    queryTime(it.getSource(), (level.dayTime / 24000L % 2147483647L).toInt())
+                }
+
+                else -> {
+                    val uuid = runCatching { UUID.fromString(uuidString) }.getOrElse { exception ->
+                        Constants.LOGGER.error(exception.localizedMessage, exception)
+                        it.source.sendFailure(Component.literal("UUID is not valid."))
+                        return@executes 0
+                    }
+                    val level = ChestLevelManager.getOrCreate(it.source.server, uuid)
+                    queryTime(it.getSource(), (level.dayTime / 24000L % 2147483647L).toInt())
+                }
+            }
+        })
+
+        timeBuilder.then(uuidWithAllArg.then(setBuilder))
+        timeBuilder.then(uuidWithAllArg.then(addBuilder))
+        timeBuilder.then(uuidArg.then(queryBuilder))
+        builder.then(timeBuilder)
         dispatcher.register(builder)
     }
 
-    private val uuidArg: RequiredArgumentBuilder<CommandSourceStack, String> by lazy {
-        Commands.argument("uuid", StringArgumentType.word()).suggests { _, builder ->
+    private val uuidArg: RequiredArgumentBuilder<CommandSourceStack, String>
+        get() = Commands.argument("uuid", StringArgumentType.word()).suggests { _, builder ->
             val tempSet = hashSetOf<String>()
             UUIDManager.getMap().keys.forEach(tempSet::add)
             UUIDManager.getInactiveList().forEach(tempSet::add)
             tempSet.forEach(builder::suggest)
-            builder.suggest("this")
+            builder.suggest(THIS)
             builder.buildFuture()
         }
-    }
+
+    private val uuidWithAllArg: RequiredArgumentBuilder<CommandSourceStack, String>
+        get() = Commands.argument("uuid", StringArgumentType.word()).suggests { _, builder ->
+            val tempSet = hashSetOf<String>()
+            UUIDManager.getMap().keys.forEach(tempSet::add)
+            UUIDManager.getInactiveList().forEach(tempSet::add)
+            tempSet.forEach(builder::suggest)
+            builder.suggest(THIS)
+            builder.suggest(ALL)
+            builder.buildFuture()
+        }
 
     private fun createChestItem(uuid: UUID): ItemStack {
         val stack = ItemStack(Items.CHEST_DIMENSION)
@@ -123,5 +281,89 @@ object ChestDimCommand {
         tag.putUUID("UUID", uuid)
         BlockItem.setBlockEntityData(stack, BlockEntityTypes.CHEST_DIMENSION, tag)
         return stack
+    }
+
+    private fun getDayTime(level: ServerLevel): Int {
+        return (level.dayTime % 24000L).toInt()
+    }
+
+    private fun queryTime(source: CommandSourceStack, time: Int): Int {
+        source.sendSuccess({ Component.translatable("commands.time.query", time) }, false)
+        return time
+    }
+
+    private fun setTime(uuidString: String, source: CommandSourceStack, time: Int): Int {
+        when (uuidString) {
+            THIS -> {
+                val uuid = ChestLevelManager.findUUIDByLevel(source.level)
+                if (uuid == null) {
+                    source.sendFailure(Component.literal("Can't find any UUID using this level."))
+                    return 0
+                }
+                val level = ChestLevelManager.getOrCreate(source.server, uuid)
+                level.dayTime = time.toLong()
+                source.sendSuccess({ Component.translatable("commands.time.set", time) }, true)
+                return getDayTime(level)
+            }
+
+            ALL -> {
+                for (serverLevel in source.server.allLevels) serverLevel.dayTime = time.toLong()
+
+                source.sendSuccess({ Component.translatable("commands.time.set", time) }, true)
+                return getDayTime(source.level)
+            }
+
+            else -> {
+                val uuid = runCatching { UUID.fromString(uuidString) }.getOrElse {
+                    Constants.LOGGER.error(it.localizedMessage, it)
+                    source.sendFailure(Component.literal("UUID is not valid."))
+                    return 0
+                }
+                val level = ChestLevelManager.getOrCreate(source.server, uuid)
+                level.dayTime = time.toLong()
+                source.sendSuccess({ Component.translatable("commands.time.set", time) }, true)
+                return getDayTime(level)
+            }
+        }
+    }
+
+    private fun addTime(uuidString: String, source: CommandSourceStack, amount: Int): Int {
+        when (uuidString) {
+            THIS -> {
+                val uuid = ChestLevelManager.findUUIDByLevel(source.level)
+                if (uuid == null) {
+                    source.sendFailure(Component.literal("Can't find any UUID using this level."))
+                    return 0
+                }
+                val level = ChestLevelManager.getOrCreate(source.server, uuid)
+                level.dayTime += amount.toLong()
+                val i = getDayTime(level)
+                source.sendSuccess({ Component.translatable("commands.time.set", i) }, true)
+                return i
+            }
+
+            ALL -> {
+                for (serverLevel in source.server.allLevels) {
+                    serverLevel.dayTime += amount.toLong()
+                }
+
+                val i = getDayTime(source.level)
+                source.sendSuccess({ Component.translatable("commands.time.set", i) }, true)
+                return i
+            }
+
+            else -> {
+                val uuid = runCatching { UUID.fromString(uuidString) }.getOrElse {
+                    Constants.LOGGER.error(it.localizedMessage, it)
+                    source.sendFailure(Component.literal("UUID is not valid."))
+                    return 0
+                }
+                val level = ChestLevelManager.getOrCreate(source.server, uuid)
+                level.dayTime += amount.toLong()
+                val i = getDayTime(level)
+                source.sendSuccess({ Component.translatable("commands.time.set", i) }, true)
+                return i
+            }
+        }
     }
 }
