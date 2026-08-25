64515b9a585351b3491c1fdc0a068060df10c27a
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-26 03:18:24 +0800

    refactor: Simplify UUID retrieval in `ChestDimCommand` by removing redundant error handling

--- src/main/kotlin/io/github/ykysnk/chestdimension/command/ChestDimCommand.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/command/ChestDimCommand.kt
@@ -30,12 +30,7 @@ object ChestDimCommand {
         builder.then(Commands.literal("create").executes { context ->
             runCatching {
                 val player = context.source.playerOrException
-                val uuid = runCatching { UUIDManager.randomUUID() }.getOrElse {
-                    Constants.LOGGER.error(it.localizedMessage, it)
-                    context.source.sendFailure(Component.literal("Invalid UUID"))
-                    return@executes 0
-                }
-
+                val uuid = UUIDManager.randomUUID()
                 val uuidComponent: MutableComponent = Component.literal(uuid.toString()).withStyle {
                     it.withUnderlined(true)
                         .withClickEvent(ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, uuid.toString()))
