ae659dd4a0f8f61049e09105aed39649c4dbd206
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-26 02:55:25 +0800

    refactor: Replace `ServerLevel` with `ChestServerLevel` in `LoadedChestWorld` for specific level handling

--- src/main/kotlin/io/github/ykysnk/chestdimension/level/LoadedChestWorld.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/level/LoadedChestWorld.kt
@@ -1,6 +1,5 @@
 package io.github.ykysnk.chestdimension.level
 
-import net.minecraft.server.level.ServerLevel
 import java.util.*
 
-data class LoadedChestWorld(val uuid: UUID, val level: ServerLevel)
+data class LoadedChestWorld(val uuid: UUID, val level: ChestServerLevel)
