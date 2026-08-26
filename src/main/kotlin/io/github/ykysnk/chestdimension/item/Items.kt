1c7e3a7b55a98e1752f396e7a439e5ed70d3f3bd
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-26 13:57:31 +0800

    feat: Introduce `ChestDimensionBlockItem` with UUID validation to prevent self-referential placement

--- src/main/kotlin/io/github/ykysnk/chestdimension/item/Items.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/item/Items.kt
@@ -12,5 +12,5 @@ object Items : RegistryHelper<Item>() {
     val CHEST_PLATFORM_FENCE: Item = register { MCItems.registerBlock(Blocks.CHEST_PLATFORM_FENCE) }
     val CHEST_PLATFORM_ENTER_PLATE: Item = register { MCItems.registerBlock(Blocks.CHEST_PLATFORM_ENTER_PLATE) }
     val TELEPORT_PRESSURE_PLATE: Item = register { MCItems.registerBlock(Blocks.TELEPORT_PRESSURE_PLATE) }
-    val CHEST_DIMENSION: Item = register { MCItems.registerBlock(Blocks.CHEST_DIMENSION) }
+    val CHEST_DIMENSION: Item = register { MCItems.registerBlock(ChestDimensionBlockItem()) }
 }
