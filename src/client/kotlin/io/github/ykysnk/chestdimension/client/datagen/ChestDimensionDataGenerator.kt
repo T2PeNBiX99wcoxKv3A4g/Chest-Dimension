66dfa74d6b87c3f7fcbf3ee403ae63316b058a97
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-27 14:11:16 +0800

    refactor: Consolidate dynamic registry providers into `DynamicRegistryProvider` and remove obsolete providers

--- src/client/kotlin/io/github/ykysnk/chestdimension/client/datagen/ChestDimensionDataGenerator.kt
+++ src/client/kotlin/io/github/ykysnk/chestdimension/client/datagen/ChestDimensionDataGenerator.kt
@@ -1,16 +1,18 @@
 package io.github.ykysnk.chestdimension.client.datagen
 
-import io.github.ykysnk.chestdimension.client.datagen.provider.*
+import io.github.ykysnk.chestdimension.client.datagen.provider.BlockLootTableProvider
+import io.github.ykysnk.chestdimension.client.datagen.provider.BlockTagProvider
+import io.github.ykysnk.chestdimension.client.datagen.provider.DynamicRegistryProvider
+import io.github.ykysnk.chestdimension.client.datagen.provider.ModelProvider
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
 
 object ChestDimensionDataGenerator : DataGeneratorEntrypoint {
-	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
-		val pack = fabricDataGenerator.createPack()
-		pack.addProvider(::DimensionTypeProvider)
-		pack.addProvider(::BiomeProvider)
-		pack.addProvider(::ModelProvider)
-		pack.addProvider(::BlockTagProvider)
-		pack.addProvider(::BlockLootTableProvider)
-	}
+    override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+        val pack = fabricDataGenerator.createPack()
+        pack.addProvider(::DynamicRegistryProvider)
+        pack.addProvider(::ModelProvider)
+        pack.addProvider(::BlockTagProvider)
+        pack.addProvider(::BlockLootTableProvider)
+    }
 }
--- src/client/kotlin/io/github/ykysnk/chestdimension/client/datagen/provider/DynamicRegistryProvider.kt
+++ src/client/kotlin/io/github/ykysnk/chestdimension/client/datagen/provider/DynamicRegistryProvider.kt
@@ -1,18 +1,22 @@
 package io.github.ykysnk.chestdimension.client.datagen.provider
 
+import io.github.ykysnk.chestdimension.level.Biomes
 import io.github.ykysnk.chestdimension.level.DimensionTypes
+import io.github.ykysnk.chestdimension.world.damagesource.DamageTypes
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider
 import net.minecraft.core.HolderLookup
 import java.util.concurrent.CompletableFuture
 
-class DimensionTypeProvider(output: FabricDataOutput, registriesFuture: CompletableFuture<HolderLookup.Provider>) :
+class DynamicRegistryProvider(output: FabricDataOutput, registriesFuture: CompletableFuture<HolderLookup.Provider>) :
     FabricDynamicRegistryProvider(output, registriesFuture) {
     override fun configure(registries: HolderLookup.Provider, entries: Entries) {
+        entries.add(Biomes.CHEST_BIOME, Biomes.ChestBiomeType)
         entries.add(DimensionTypes.CHEST, DimensionTypes.ChestDimensionType)
+        entries.add(DamageTypes.EXPLOSION_BY_CHEST, DamageTypes.ExplosionByChestType)
     }
 
     override fun getName(): String {
-        return "Chest Dimension Dynamic Registries"
+        return "Chest Biome Dynamic Registries"
     }
 }
