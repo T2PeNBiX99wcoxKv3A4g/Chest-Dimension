9ebf1c7ddf3c076413d12ef89ee448d6d541ad12
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-29 14:07:18 +0800

    refactor: Use collection factory methods for `levels` and `inactiveLevels` initialization in `Levels`

--- src/main/kotlin/io/github/ykysnk/chestdimension/data/Levels.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/data/Levels.kt
@@ -3,6 +3,9 @@ package io.github.ykysnk.chestdimension.data
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Levels(var levels: HashMap<String, LevelData> = HashMap(), var inactiveLevels: HashSet<String> = HashSet()) {
+data class Levels(
+    var levels: HashMap<String, LevelData> = hashMapOf(),
+    var inactiveLevels: HashSet<String> = hashSetOf()
+) {
     fun deepCopy(): Levels = Levels(HashMap(levels), HashSet(inactiveLevels))
 }
