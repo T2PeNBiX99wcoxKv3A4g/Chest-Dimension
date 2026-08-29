e9e3b3d4596a83e0c8a18faa67d555c9dafc4b2a
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-29 15:12:58 +0800

    feat: Add time synchronization handling for `ChestServerLevel` to support `freezeTime` and daylight rule adjustments

--- src/main/java/io/github/ykysnk/chestdimension/mixin/MinecraftServerMixin.java
+++ src/main/java/io/github/ykysnk/chestdimension/mixin/MinecraftServerMixin.java
@@ -1,10 +1,15 @@
 package io.github.ykysnk.chestdimension.mixin;
 
 import io.github.ykysnk.chestdimension.level.ChestLevelManager;
+import io.github.ykysnk.chestdimension.level.ChestServerLevel;
+import net.minecraft.network.protocol.game.ClientboundSetTimePacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.progress.ChunkProgressListener;
+import net.minecraft.server.players.PlayerList;
+import net.minecraft.world.level.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,6 +21,9 @@ import java.util.Collection;
 @SuppressWarnings({"AbstractClassWithoutAbstractMethods", "AbstractClassNeverImplemented", "MethodMayBeStatic"})
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerMixin {
+    @Shadow
+    private PlayerList playerList;
+
     @Inject(method = "createLevels", at = @At("TAIL"))
     private void createLevels(ChunkProgressListener listener, CallbackInfo ci) {
         ChestLevelManager.INSTANCE.load((MinecraftServer) (Object) this, listener);
@@ -24,5 +32,12 @@ public abstract class MinecraftServerMixin {
     @Inject(method = "getAllLevels", at = @At("RETURN"), cancellable = true)
     private void getAllLevels(CallbackInfoReturnable<Iterable<ServerLevel>> cir) {
         cir.setReturnValue(new ArrayList<>((Collection<ServerLevel>) cir.getReturnValue()));
+    }
+
+    @Inject(method = "synchronizeTime", at = @At("HEAD"), cancellable = true)
+    private void synchronizeTime(ServerLevel level, CallbackInfo ci) {
+        if (!(level instanceof ChestServerLevel chestServerLevel)) return;
+        playerList.broadcastAll(new ClientboundSetTimePacket(level.getGameTime(), level.getDayTime(), !chestServerLevel.getChestServerLevelData().getFreezeTime() && level.getGameRules().getBoolean(GameRules.RULE_DAYLIGHT)), level.dimension());
+        ci.cancel();
     }
 }
