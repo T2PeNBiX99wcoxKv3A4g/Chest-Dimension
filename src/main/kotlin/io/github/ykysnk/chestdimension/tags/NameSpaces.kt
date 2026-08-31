9edca0a86eeec5128855bf1ddd8b523ce65d8f34
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-31 22:16:59 +0800

    feat: Add `NameSpaces` enum for managing namespace identifiers

---
+++ src/main/kotlin/io/github/ykysnk/chestdimension/tags/NameSpaces.kt
@@ -0,0 +1,8 @@
+package io.github.ykysnk.chestdimension.tags
+
+import io.github.ykysnk.chestdimension.Constants
+
+enum class NameSpaces(val id: String) {
+    MOD(Constants.MOD_ID),
+    FORGE("c")
+}
