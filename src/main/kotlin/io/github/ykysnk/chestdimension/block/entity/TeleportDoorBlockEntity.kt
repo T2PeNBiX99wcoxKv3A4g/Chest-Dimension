8dbffa1c95bd0b2c8f0a2023a49e23a046a3ee5e
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-01 09:39:27 +0800

    feat: Add `uuid` field to `TeleportDoorBlockEntity` initialized with `TeleportManager.randomUUID()`

--- src/main/kotlin/io/github/ykysnk/chestdimension/block/entity/TeleportDoorBlockEntity.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/block/entity/TeleportDoorBlockEntity.kt
@@ -1,8 +1,13 @@
 package io.github.ykysnk.chestdimension.block.entity
 
+import io.github.ykysnk.chestdimension.level.TeleportManager
 import net.minecraft.core.BlockPos
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
+import java.util.*
 
 class TeleportDoorBlockEntity(pos: BlockPos, blockState: BlockState) :
-    BlockEntity(BlockEntityTypes.TELEPORT_DOOR, pos, blockState)
+    BlockEntity(BlockEntityTypes.TELEPORT_DOOR, pos, blockState) {
+    var uuid: UUID = TeleportManager.randomUUID()
+        private set
+}
