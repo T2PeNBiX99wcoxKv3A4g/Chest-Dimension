4ad21a7c67484895829bf5733e85e9021495a84a
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-02 17:33:43 +0800

    feat: Add server lifecycle event handling to `TaskPool` for improved task management

--- src/main/kotlin/io/github/ykysnk/chestdimension/utils/TaskPool.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/utils/TaskPool.kt
@@ -1,18 +1,31 @@
 package io.github.ykysnk.chestdimension.utils
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
 
 object TaskPool {
     private val tasks = mutableListOf<() -> Unit>()
+    private var isStop = false
 
     fun run(task: () -> Unit) {
         tasks.add(task)
     }
 
+    private fun runTasks() {
+        if (isStop) return
+        tasks.forEach { it() }
+        tasks.clear()
+    }
+
     init {
         ServerTickEvents.END_SERVER_TICK.register {
-            tasks.forEach { it() }
-            tasks.clear()
+            runTasks()
+        }
+        ServerLifecycleEvents.SERVER_STARTING.register {
+            isStop = false
+        }
+        ServerLifecycleEvents.SERVER_STOPPING.register {
+            isStop = true
         }
     }
 }
