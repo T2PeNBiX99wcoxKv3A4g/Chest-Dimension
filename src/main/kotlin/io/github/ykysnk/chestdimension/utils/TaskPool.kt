0e9c562efcad17f22d05ee05222c17fafca6a94f
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-02 19:38:39 +0800

    feat: Add delayed task execution in `TaskPool` and track ticks for scheduling

--- src/main/kotlin/io/github/ykysnk/chestdimension/utils/TaskPool.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/utils/TaskPool.kt
@@ -1,31 +1,39 @@
 package io.github.ykysnk.chestdimension.utils
 
+import io.github.ykysnk.chestdimension.data.TaskData
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
 
 object TaskPool {
-    private val tasks = mutableListOf<() -> Unit>()
+    private val tasks: MutableList<TaskData> = mutableListOf()
     private var isStop = false
+    private var tickCount = 0L
 
-    fun run(task: () -> Unit) = tasks.add(task)
+    fun run(task: () -> Unit, delayTick: Long = 0L) {
+        tasks.add(TaskData(tickCount + delayTick, task))
+    }
 
     private fun runTasks() {
         val currentTasks = tasks.toList()
         tasks.clear()
-        currentTasks.forEach { it() }
+        currentTasks.forEach {
+            if (it.nextTick <= tickCount || isStop) return@forEach
+            it.task()
+        }
     }
 
     init {
         ServerTickEvents.END_SERVER_TICK.register {
             if (isStop) return@register
             runTasks()
+            tickCount++
         }
         ServerLifecycleEvents.SERVER_STARTING.register {
             isStop = false
         }
         ServerLifecycleEvents.SERVER_STOPPING.register {
             isStop = true
-            tasks.clear()
+            tickCount = 0L
         }
     }
 }
