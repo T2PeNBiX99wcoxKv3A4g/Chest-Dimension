037c2b8fcf173e3ee03f43eded0942a0470b7209
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-02 21:46:57 +0800

    refactor: Optimize teleportation logic in `TeleportDoorBlock` by integrating `TaskPool` and adjusting bounding box checks

--- src/main/kotlin/io/github/ykysnk/chestdimension/block/TeleportDoorBlock.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/block/TeleportDoorBlock.kt
@@ -4,15 +4,14 @@ import io.github.ykysnk.chestdimension.block.entity.BlockEntityTypes
 import io.github.ykysnk.chestdimension.block.entity.TeleportDoorBlockEntity
 import io.github.ykysnk.chestdimension.item.Items
 import io.github.ykysnk.chestdimension.level.TeleportManager
+import io.github.ykysnk.chestdimension.utils.TaskPool
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.server.level.ServerLevel
-import net.minecraft.util.RandomSource
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.entity.Entity
-import net.minecraft.world.entity.EntitySelector
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.ItemStack
@@ -31,7 +30,7 @@ import net.minecraft.world.phys.BlockHitResult
 class TeleportDoorBlock(properties: Properties, type: BlockSetType) : DoorBlock(properties, type), EntityBlock {
     companion object {
         private const val DOOR_THICKNESS = 3.0 / 16.0
-        private const val TOUCH_THICKNESS = 0.5 / 16.0
+        private const val TOUCH_THICKNESS = 1 / 16.0
         private const val TOUCH_OFFSET = DOOR_THICKNESS - TOUCH_THICKNESS
         private val SOUTH_TOUCH_AABB = AABB(0.0, 0.0, TOUCH_OFFSET, 1.0, 1.0, TOUCH_OFFSET + TOUCH_THICKNESS)
         private val NORTH_TOUCH_AABB =
@@ -75,18 +74,16 @@ class TeleportDoorBlock(properties: Properties, type: BlockSetType) : DoorBlock(
         if (level.isClientSide) return
         (level as? ServerLevel)?.apply ServerLevel@{
             val box = getTouchAABB(state, pos)
-            val list = getEntitiesOfClass(Entity::class.java, box, EntitySelector.NO_SPECTATORS)
+
             getLowerEntity(state, this, pos)?.apply {
-                list.forEach { enterDoor(it) }
-                level.scheduleTick(pos, this@TeleportDoorBlock, 2)
+                if (entity.boundingBox.intersects(box))
+                    enterDoor(entity)
+                TaskPool.run(1) {
+                    val blockEntity = getLowerEntity(state, level, pos) ?: return@run
+                    blockEntity.startTeleport()
+                }
             }
         }
-    }
-
-    @Deprecated("Deprecated in Java")
-    override fun tick(state: BlockState, level: ServerLevel, pos: BlockPos, random: RandomSource) {
-        val blockEntity = getLowerEntity(state, level, pos) ?: return
-        blockEntity.startTeleport()
     }
 
     override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity = TeleportDoorBlockEntity(pos, state)
@@ -133,9 +130,12 @@ class TeleportDoorBlock(properties: Properties, type: BlockSetType) : DoorBlock(
     ) {
         if (!state.`is`(newState.block)) {
             val blockEntity = level.getBlockEntity(pos)
-            if (blockEntity is TeleportDoorBlockEntity && state.getValue(HALF) == DoubleBlockHalf.LOWER) {
-                TeleportManager.removePosition(blockEntity.uuid)
-                TeleportManager.save()
+            if (blockEntity is TeleportDoorBlockEntity) {
+                TeleportDoorBlockEntity.blockPosList.remove(pos)
+                if (!level.isClientSide && state.getValue(HALF) == DoubleBlockHalf.LOWER) {
+                    TeleportManager.removePosition(blockEntity.uuid)
+                    TeleportManager.save()
+                }
             }
         }
 
