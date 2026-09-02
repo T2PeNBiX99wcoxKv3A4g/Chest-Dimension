607f7143c5a0deb82778c671d074dff0eebcfbf7
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-02 19:02:05 +0800

    feat: Add coroutine support in `TaskPool` for non-blocking task execution and improve lifecycle handling

--- src/main/kotlin/io/github/ykysnk/chestdimension/utils/TaskPool.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/utils/TaskPool.kt
@@ -1,19 +1,23 @@
 package io.github.ykysnk.chestdimension.utils
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
 
 object TaskPool {
     private val tasks = mutableListOf<() -> Unit>()
+    private val scope: CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
     private var isStop = false
 
-    fun run(task: () -> Unit) {
-        tasks.add(task)
-    }
+    fun run(task: () -> Unit) = tasks.add(task)
 
     private fun runTasks() {
-        tasks.forEach { it() }
+        val currentTasks = tasks.toList()
         tasks.clear()
+        currentTasks.forEach { scope.launch { it() } }
     }
 
     init {
@@ -26,6 +30,7 @@ object TaskPool {
         }
         ServerLifecycleEvents.SERVER_STOPPING.register {
             isStop = true
+            tasks.clear()
         }
     }
 }
