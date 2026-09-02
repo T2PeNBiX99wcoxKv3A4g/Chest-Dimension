b5f5f8e37710c5544887cb5533ca555c8c5ae3b3
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-02 16:55:10 +0800

    feat: Introduce `TaskPool` utility with server-tick-based task execution

---
+++ src/main/kotlin/io/github/ykysnk/chestdimension/utils/TaskPool.kt
@@ -0,0 +1,18 @@
+package io.github.ykysnk.chestdimension.utils
+
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
+
+object TaskPool {
+    private val tasks = mutableListOf<() -> Unit>()
+
+    fun add(task: () -> Unit) {
+        tasks.add(task)
+    }
+
+    init {
+        ServerTickEvents.END_SERVER_TICK.register {
+            tasks.forEach { it() }
+            tasks.clear()
+        }
+    }
+}
