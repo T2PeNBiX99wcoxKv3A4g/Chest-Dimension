5813b45076dbc189abba37607f9aa99fbdc84e05
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-02 13:21:08 +0800

    refactor: Implement `IsEmpty` interface in `TeleportInfo` for empty state checks

--- src/main/kotlin/io/github/ykysnk/chestdimension/level/data/TeleportInfo.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/level/data/TeleportInfo.kt
@@ -1,5 +1,6 @@
 package io.github.ykysnk.chestdimension.level.data
 
+import io.github.ykysnk.chestdimension.data.IsEmpty
 import io.github.ykysnk.chestdimension.data.NbtLoad
 import io.github.ykysnk.chestdimension.data.NbtSave
 import io.github.ykysnk.chestdimension.utils.getCompoundOrNull
@@ -7,13 +8,16 @@ import io.github.ykysnk.chestdimension.utils.getUUIDOrNull
 import net.minecraft.nbt.CompoundTag
 import java.util.*
 
-data class TeleportInfo(val linkUUID: UUID? = null, val dimensionPosition: DimensionPosition? = null) : NbtSave {
+data class TeleportInfo(val linkUUID: UUID? = null, val dimensionPosition: DimensionPosition? = null) : NbtSave,
+    IsEmpty {
     override fun save(): CompoundTag {
         val tag = CompoundTag()
         linkUUID?.let { tag.putUUID("linkUUID", it) }
         dimensionPosition?.let { tag.put("dimensionPosition", it.save()) }
         return tag
     }
+
+    override fun isEmpty() = linkUUID == null && dimensionPosition == null
 
     companion object : NbtLoad<TeleportInfo> {
         override fun load(tag: CompoundTag): TeleportInfo {
