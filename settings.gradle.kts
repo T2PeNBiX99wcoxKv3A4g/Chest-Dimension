92d329bc0c204cf0fe88e1062c03d4b5785c74c1
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-03 12:13:18 +0800

    style: Reformat `settings.gradle.kts` for consistent indentation

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,17 @@
 pluginManagement {
-	repositories {
-		maven("https://maven.fabricmc.net/") {
-			name = "Fabric"
-		}
-		mavenCentral()
-		gradlePluginPortal()
-	}
+    repositories {
+        maven("https://maven.fabricmc.net/") {
+            name = "Fabric"
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
 
-	plugins {
-		id("net.fabricmc.fabric-loom-remap") version providers.gradleProperty("loom_version")
-		kotlin("jvm") version providers.gradleProperty("jvm_version")
-		kotlin("plugin.serialization") version providers.gradleProperty("jvm_version")
-	}
+    plugins {
+        id("net.fabricmc.fabric-loom-remap") version providers.gradleProperty("loom_version")
+        kotlin("jvm") version providers.gradleProperty("jvm_version")
+        kotlin("plugin.serialization") version providers.gradleProperty("jvm_version")
+    }
 }
 
 // Should match your modid
