d71b2c14ef50f2ba0190a6fc50ce7da89c640e31
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-04 16:52:18 +0800

    feat: enhance `ServerLevelMixin` to handle custom chunk generators and propagate world options in `ChestServerLevel`

--- src/main/java/io/github/ykysnk/chestdimension/mixin/ServerLevelMixin.java
+++ src/main/java/io/github/ykysnk/chestdimension/mixin/ServerLevelMixin.java
@@ -6,6 +6,8 @@ import io.github.ykysnk.chestdimension.Constants;
 import io.github.ykysnk.chestdimension.level.ChestServerLevel;
 import io.github.ykysnk.chestdimension.level.ChestServerLevelContext;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.dimension.LevelStem;
 import net.minecraft.world.level.levelgen.WorldOptions;
 import net.minecraft.world.level.storage.WorldData;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,8 +15,22 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ServerLevel.class)
 public abstract class ServerLevelMixin {
+    @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/dimension/LevelStem;generator()Lnet/minecraft/world/level/chunk/ChunkGenerator;"))
+    private ChunkGenerator newArrayList(LevelStem instance, Operation<ChunkGenerator> original) {
+        var generator = original.call(instance);
+        if ((Object) this instanceof ChestServerLevel chestServerLevel) {
+            var options = ChestServerLevelContext.worldOptions.get();
+            if (options == null) {
+                Constants.LOGGER.warn("Chest dimension world options not found in context");
+                return generator;
+            }
+            chestServerLevel.levelWorldOptions = options;
+        }
+        return generator;
+    }
+
     @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/storage/WorldData;worldGenOptions()Lnet/minecraft/world/level/levelgen/WorldOptions;"))
-    private WorldOptions init(WorldData instance, Operation<WorldOptions> original) {
+    private WorldOptions worldGenOptions(WorldData instance, Operation<WorldOptions> original) {
         var originalOptions = original.call(instance);
         if ((Object) this instanceof ChestServerLevel) {
             var options = ChestServerLevelContext.worldOptions.get();
