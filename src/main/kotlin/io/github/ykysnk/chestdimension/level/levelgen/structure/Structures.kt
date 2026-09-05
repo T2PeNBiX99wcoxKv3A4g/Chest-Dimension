9031b7cbb9942d78a8417640dddb2d455ba9ebba
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-05 21:03:34 +0800

    refactor: update biome reference in Structures bootstrap logic
    - Switched undefined biome reference to CHES_PLATFORM_BIOME as a temporary adjustment.

--- src/main/kotlin/io/github/ykysnk/chestdimension/level/levelgen/structure/Structures.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/level/levelgen/structure/Structures.kt
@@ -25,7 +25,7 @@ object Structures {
     fun bootstrap(context: BootstapContext<Structure>) {
         val biomes: HolderGetter<Biome> = context.lookup(Registries.BIOME)
         val pools: HolderGetter<StructureTemplatePool> = context.lookup(Registries.TEMPLATE_POOL)
-        val biomeSet = HolderSet.direct(biomes.getOrThrow(Biomes.CHEST_UNDEFINED_BIOME))
+        val biomeSet = HolderSet.direct(biomes.getOrThrow(Biomes.CHEST_PLATFORM_BIOME)) // TODO: Temp Disable
         val settings = Structure.StructureSettings(
             biomeSet,
             emptyMap(),
