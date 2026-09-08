4a10242399caa8c9383e32e56c565b2c1b7135ac
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-08 18:35:31 +0800

    tweak: add torches to Small Shelter loot table and adjust rolls distribution

--- src/client/kotlin/io/github/ykysnk/chestdimension/client/datagen/provider/SimpleLootTableProvider.kt
+++ src/client/kotlin/io/github/ykysnk/chestdimension/client/datagen/provider/SimpleLootTableProvider.kt
@@ -28,13 +28,16 @@ class SimpleLootTableProvider(output: FabricDataOutput) :
                 ).add(
                     LootItem.lootTableItem(MCItems.GOLD_NUGGET).setWeight(5)
                         .apply(SetItemCountFunction.setCount(UniformGenerator.between(2.0f, 8.0f)))
+                ).add(
+                    LootItem.lootTableItem(MCItems.TORCH).setWeight(10)
+                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(10.0f, 20.0f)))
                 )
             )
         )
         output.accept(
             LootTables.SMALL_SHELTER,
             LootTable.lootTable().withPool(
-                LootPool.lootPool().setRolls(UniformGenerator.between(2.0f, 5.0f)).add(
+                LootPool.lootPool().setRolls(UniformGenerator.between(3.0f, 6.0f)).add(
                     LootItem.lootTableItem(MCItems.IRON_INGOT).setWeight(10)
                         .apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0f, 5.0f)))
                 ).add(
@@ -49,6 +52,9 @@ class SimpleLootTableProvider(output: FabricDataOutput) :
                 ).add(
                     LootItem.lootTableItem(MCItems.DIAMOND).setWeight(2)
                         .apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0f, 2.0f)))
+                ).add(
+                    LootItem.lootTableItem(MCItems.TORCH).setWeight(20)
+                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(32.0f, 64.0f)))
                 )
             )
         )
