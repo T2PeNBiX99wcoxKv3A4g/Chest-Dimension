856c7282649bc940a28fcc169c08ffd5d890e4ad
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-10 18:24:05 +0800

    tweak: update `CHEST_UNDEFINED` music timing to play from 6000 to 24000 ticks

--- src/main/kotlin/io/github/ykysnk/chestdimension/sounds/Musics.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/sounds/Musics.kt
@@ -4,5 +4,5 @@ import net.minecraft.sounds.Music
 
 object Musics {
     @JvmField
-    val CHEST_UNDEFINED = Music(SoundEvents.MUSIC_CHEST_UNDEFINED, 0, 0, true)
+    val CHEST_UNDEFINED = Music(SoundEvents.MUSIC_CHEST_UNDEFINED, 6000, 24000, true)
 }
