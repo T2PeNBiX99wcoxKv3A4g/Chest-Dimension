03a4e5079240f09d66e49bc2130c5f2f1560a3da
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-10 17:51:54 +0800

    refactor: centralize `CHEST_UNDEFINED` music in `Musics` object and update biome usage

---
+++ src/main/kotlin/io/github/ykysnk/chestdimension/sounds/Musics.kt
@@ -0,0 +1,8 @@
+package io.github.ykysnk.chestdimension.sounds
+
+import net.minecraft.sounds.Music
+
+object Musics {
+    @JvmField
+    val CHEST_UNDEFINED = Music(SoundEvents.MUSIC_CHEST_UNDEFINED, 0, 0, true)
+}
