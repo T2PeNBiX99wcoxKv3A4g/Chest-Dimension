c0abb5a2d169356f73cfe7bc451525c655ddad3a
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-11 11:54:00 +0800

    refactor: use `NameSpaces.MOD` in `WeatherTimeControllerScreen` for consistency

--- src/client/kotlin/io/github/ykysnk/chestdimension/client/world/inventory/WeatherTimeControllerScreen.kt
+++ src/client/kotlin/io/github/ykysnk/chestdimension/client/world/inventory/WeatherTimeControllerScreen.kt
@@ -4,7 +4,7 @@ import io.github.ykysnk.chestdimension.client.extensions.freezeTime
 import io.github.ykysnk.chestdimension.client.extensions.freezeWeather
 import io.github.ykysnk.chestdimension.client.extensions.sendSetTime
 import io.github.ykysnk.chestdimension.client.extensions.sendWeather
-import io.github.ykysnk.chestdimension.id
+import io.github.ykysnk.chestdimension.tags.NameSpaces
 import io.github.ykysnk.chestdimension.world.Network
 import io.github.ykysnk.chestdimension.world.WeatherType
 import io.github.ykysnk.chestdimension.world.inventory.WeatherTimeControllerMenu
@@ -18,7 +18,7 @@ class WeatherTimeControllerScreen(menu: WeatherTimeControllerMenu, inventory: In
     AbstractContainerScreen<WeatherTimeControllerMenu>(menu, inventory, title) {
     companion object {
         private const val ROWS = 6
-        private val INVENTORY_TEXTURE = id("textures/gui/container/weather_time_controller.png")
+        private val INVENTORY_TEXTURE = NameSpaces.MOD("textures/gui/container/weather_time_controller.png")
     }
 
     private lateinit var timeSlider: TimeSlider
