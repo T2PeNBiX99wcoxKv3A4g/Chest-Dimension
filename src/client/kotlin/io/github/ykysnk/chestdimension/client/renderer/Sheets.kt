433b65a967eabcf09364dcdd1840c908b5e145b8
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-11 11:53:15 +0800

    refactor: use `NameSpaces.MOD` in `Sheets` for consistency

--- src/client/kotlin/io/github/ykysnk/chestdimension/client/renderer/Sheets.kt
+++ src/client/kotlin/io/github/ykysnk/chestdimension/client/renderer/Sheets.kt
@@ -1,13 +1,13 @@
 package io.github.ykysnk.chestdimension.client.renderer
 
-import io.github.ykysnk.chestdimension.id
+import io.github.ykysnk.chestdimension.tags.NameSpaces
 import net.minecraft.client.renderer.Sheets
 import net.minecraft.client.resources.model.Material
 
 object Sheets {
     @Suppress("SameParameterValue")
     private fun chestMaterial(chestName: String): Material {
-        return Material(Sheets.CHEST_SHEET, id("entity/chest/$chestName"))
+        return Material(Sheets.CHEST_SHEET, NameSpaces.MOD("entity/chest/$chestName"))
     }
 
     @JvmField
