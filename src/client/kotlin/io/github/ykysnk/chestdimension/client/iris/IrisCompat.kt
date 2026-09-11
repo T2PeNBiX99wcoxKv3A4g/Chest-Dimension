82ee7288c30f65fb76c119414aeee2fd7d303af3
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-11 13:20:12 +0800

    refactor: use `NameSpaces.MOD` in `IrisCompat` for consistency

--- src/client/kotlin/io/github/ykysnk/chestdimension/client/iris/IrisCompat.kt
+++ src/client/kotlin/io/github/ykysnk/chestdimension/client/iris/IrisCompat.kt
@@ -1,28 +1,23 @@
 package io.github.ykysnk.chestdimension.client.iris
 
 import io.github.ykysnk.chestdimension.compat.ModCompat
-import io.github.ykysnk.chestdimension.utils.SimpleResourceLocation
+import io.github.ykysnk.chestdimension.tags.NameSpaces
 import net.irisshaders.iris.Iris
 import net.irisshaders.iris.uniforms.SystemTimeUniforms
 
 object IrisCompat : ModCompat("iris") {
-    private const val MOD_ID = "chest-dimension"
     fun getCurrentPack() = Iris.getCurrentPack()
     val frameTimeCounter
         get() = SystemTimeUniforms.TIMER.frameTimeCounter
 
     override fun initialize() {
-        IrisBlockRegistry.register(
-            SimpleResourceLocation("end_portal"),
-            SimpleResourceLocation(MOD_ID, "teleport_door")
-        )
-        IrisBlockRegistry.register(
-            SimpleResourceLocation("glass"),
-            SimpleResourceLocation(MOD_ID, "blast_resistant_glass")
-        )
-        IrisBlockRegistry.register(
-            SimpleResourceLocation("glass_pane"),
-            SimpleResourceLocation(MOD_ID, "blast_resistant_glass_pane")
-        )
+        IrisBlockRegistry.apply {
+            register(NameSpaces.MINECRAFT.pathSimple("end_portal"), NameSpaces.MOD.pathSimple("teleport_door"))
+            register(NameSpaces.MINECRAFT.pathSimple("glass"), NameSpaces.MOD.pathSimple("blast_resistant_glass"))
+            register(
+                NameSpaces.MINECRAFT.pathSimple("glass_pane"),
+                NameSpaces.MOD.pathSimple("blast_resistant_glass_pane")
+            )
+        }
     }
 }
