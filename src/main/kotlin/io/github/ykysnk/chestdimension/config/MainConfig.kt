a2ae33fda122e7c335405605ef007f7f6001f123
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-11 11:56:21 +0800

    refactor: use `NameSpaces.MOD` in `MainConfig` for consistency

--- src/main/kotlin/io/github/ykysnk/chestdimension/config/MainConfig.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/config/MainConfig.kt
@@ -1,9 +1,9 @@
 package io.github.ykysnk.chestdimension.config
 
-import io.github.ykysnk.chestdimension.id
+import io.github.ykysnk.chestdimension.tags.NameSpaces
 import me.fzzyhmstrs.fzzy_config.config.Config
 
-class MainConfig : Config(id("main_config")) {
+class MainConfig : Config(NameSpaces.MOD("main_config")) {
     var teleportDoorTeleportToUndefined = true
     var automaticCloseTeleportDoor = false
 }
