d7d59d1d783ae4006bea2cf738a37588c924b846
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-11 11:51:00 +0800

    refactor: use `NameSpaces.MOD` in `LootTables` for consistency

--- src/main/kotlin/io/github/ykysnk/chestdimension/level/levelgen/structure/LootTables.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/level/levelgen/structure/LootTables.kt
@@ -1,8 +1,8 @@
 package io.github.ykysnk.chestdimension.level.levelgen.structure
 
-import io.github.ykysnk.chestdimension.Constants.id
+import io.github.ykysnk.chestdimension.tags.NameSpaces
 
 object LootTables {
-    val DEATH_BODY_CHEST = id("chests/death_body_chest")
-    val SMALL_SHELTER = id("chests/small_shelter")
+    val DEATH_BODY_CHEST = NameSpaces.MOD("chests/death_body_chest")
+    val SMALL_SHELTER = NameSpaces.MOD("chests/small_shelter")
 }
