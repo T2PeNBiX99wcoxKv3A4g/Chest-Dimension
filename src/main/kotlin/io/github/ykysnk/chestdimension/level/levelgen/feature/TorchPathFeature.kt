a237725404ce38396ac80dfb7d143a7e4cfc641d
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-12 23:48:12 +0800

    fix: pass origin y-coordinate to placeTorch to ensure correct placement logic

--- src/main/kotlin/io/github/ykysnk/chestdimension/level/levelgen/feature/TorchPathFeature.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/level/levelgen/feature/TorchPathFeature.kt
@@ -104,7 +104,7 @@ class TorchPathFeature(codec: Codec<TorchPathConfiguration>) : Feature<TorchPath
                      * The path itself is outside this chunk.
                      */
                     if (z !in minZ..maxZ) continue
-                    if (placeTorch(level, x, z)) placed = true
+                    if (placeTorch(level, x, z, origin.y)) placed = true
                 }
             }
         } else {
@@ -138,7 +138,7 @@ class TorchPathFeature(codec: Codec<TorchPathConfiguration>) : Feature<TorchPath
                      * The path itself is outside this chunk.
                      */
                     if (x !in minX..maxX) continue
-                    if (placeTorch(level, x, z)) placed = true
+                    if (placeTorch(level, x, z, origin.y)) placed = true
                 }
             }
         }
@@ -273,8 +273,8 @@ class TorchPathFeature(codec: Codec<TorchPathConfiguration>) : Feature<TorchPath
      *
      * This only modifies the current generation area.
      */
-    private fun placeTorch(level: WorldGenLevel, x: Int, z: Int): Boolean {
-        var y = level.maxBuildHeight
+    private fun placeTorch(level: WorldGenLevel, x: Int, z: Int, y: Int): Boolean {
+        var y = y
         while (y > level.minBuildHeight && level.getBlockState(BlockPos(x, y - 1, z)).isAir) y--
         val torchPos = BlockPos(x, y, z)
         if (!level.getBlockState(torchPos).isAir) return false
