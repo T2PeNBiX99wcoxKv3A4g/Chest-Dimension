5e094e08980d533cfaf77ba3e1af8df46dd4d430
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-15 00:37:38 +0800

    refactor: reformat `AFTER_CREATE_LEVEL` event declaration for improved readability

--- src/main/kotlin/io/github/ykysnk/chestdimension/event/UtilsEvents.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/event/UtilsEvents.kt
@@ -7,11 +7,12 @@ import net.minecraft.server.level.progress.ChunkProgressListener
 
 object UtilsEvents {
     @JvmField
-    val AFTER_CREATE_LEVEL: Event<AfterCreateLevel> = EventFactory.createArrayBacked(AfterCreateLevel::class.java) { callbacks ->
-        AfterCreateLevel { server, listener ->
-            callbacks.forEach { it.onAfterCreateLevel(server, listener) }
+    val AFTER_CREATE_LEVEL: Event<AfterCreateLevel> =
+        EventFactory.createArrayBacked(AfterCreateLevel::class.java) { callbacks ->
+            AfterCreateLevel { server, listener ->
+                callbacks.forEach { it.onAfterCreateLevel(server, listener) }
+            }
         }
-    }
 
     fun interface AfterCreateLevel {
         fun onAfterCreateLevel(server: MinecraftServer, listener: ChunkProgressListener)
