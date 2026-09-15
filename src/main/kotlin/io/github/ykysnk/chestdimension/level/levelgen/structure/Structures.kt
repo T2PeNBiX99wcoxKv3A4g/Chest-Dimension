273ba6c290c0e6296da75acf672db57febf15c41
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-15 16:38:26 +0800

    refactor: adjust structure heights for `DeathBodyChest` and `SmallShelter`

--- src/main/kotlin/io/github/ykysnk/chestdimension/level/levelgen/structure/Structures.kt
+++ src/main/kotlin/io/github/ykysnk/chestdimension/level/levelgen/structure/Structures.kt
@@ -42,7 +42,7 @@ object Structures {
                 settings,
                 pools.getOrThrow(TemplatePools.DEATH_BODY_CHEST_MAIN_POOL),
                 7,
-                ConstantHeight.of(VerticalAnchor.absolute(-2)),
+                ConstantHeight.of(VerticalAnchor.absolute(0)),
                 false,
                 Heightmap.Types.WORLD_SURFACE_WG
             )
@@ -54,7 +54,7 @@ object Structures {
                 settings,
                 pools.getOrThrow(TemplatePools.SMALL_SHELTER_POOL),
                 7,
-                ConstantHeight.of(VerticalAnchor.absolute(0)),
+                ConstantHeight.of(VerticalAnchor.absolute(-1)),
                 false,
                 Heightmap.Types.WORLD_SURFACE_WG
             )
